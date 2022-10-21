bc4216410d5ab7da054acc4a72acc08223d1c7ad
Ritusmoi Kaushik <ritusmoi.kaushik@bohniman.com>
2022-10-21 12:43:56 +0530

    Program To Add Two Numbers Added

--- src/AddTwoNumbers.java
+++ src/AddTwoNumbers.java
@@ -1,3 +1,12 @@
+// Java Program To Add Two Numbers
+
 public class AddTwoNumbers {
-    
+    public static void main(String[] args) {
+        
+        int a = 20 , b = 30;
+
+        int sum = a + b;
+
+        System.out.println("Sum of numbers "+a+" and "+b+" is "+sum);
+    }
 }
---
+++ src/AddTwoNumbersUsingScanner.java
@@ -0,0 +1,12 @@
+// Java Program To Add Two Numbers Taking Input From User
+
+public class AddTwoNumbersUsingScanner {
+    public static void main(String[] args) {
+        
+        int a = 20 , b = 30;
+
+        int sum = a + b;
+
+        System.out.println("Sum of numbers "+a+" and "+b+" = "+sum);
+    }
+}
