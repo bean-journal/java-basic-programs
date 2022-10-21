ef85bae481b65c45d12f07494e77985f501dea7a
Ritusmoi Kaushik <ritusmoi.kaushik@bohniman.com>
2022-10-21 14:25:26 +0530

    Program to add two numbers taking input from user added

--- src/AddTwoNumbersUsingScanner.java
+++ src/AddTwoNumbersUsingScanner.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 // Java Program To Add Two Numbers Taking Input From User
 
 public class AddTwoNumbersUsingScanner {
     public static void main(String[] args) {
         
-        int a = 20 , b = 30;
+        int a , b, sum;
 
-        int sum = a + b;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first number ");
+
+        a = scanner.nextInt();
+
+        System.out.println("Enter second number ");
+
+        b = scanner.nextInt();
+
+        scanner.close();
+
+        //ADDITION
+        sum = a + b;
 
         System.out.println("Sum of numbers "+a+" and "+b+" = "+sum);
     }
